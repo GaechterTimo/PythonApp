f68d864df7778c0744377bb36b9f05c0f2b4c394
GaechterTimo <148241516+GaechterTimo@users.noreply.github.com>
2023-10-23 22:45:33 +0200

    Update locustfile.py

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,22 +1,44 @@
 from locust import HttpUser, task, between
 
-class MyUser(HttpUser):
+class CalculatorUser(HttpUser):
     wait_time = between(1, 5)  # Set wait time between tasks
 
     @task
-    def visit_homepage(self):
-        self.client.get("/")
+    def add_numbers(self):
+        response = self.client.get("/add?value=5")  # Simulate adding numbers
+        if response.status_code == 200:
+            self.locust.log_success("add_numbers", response.elapsed.total_seconds())
+        else:
+            self.locust.log_failure("add_numbers", response.text)
 
     @task
-    def perform_search(self):
-        self.client.get("/search?query=test")
+    def subtract_numbers(self):
+        response = self.client.get("/subtract?value=2")  # Simulate subtracting numbers
+        if response.status_code == 200:
+            self.locust.log_success("subtract_numbers", response.elapsed.total_seconds())
+        else:
+            self.locust.log_failure("subtract_numbers", response.text)
 
-    # Add more tasks as needed
+    @task
+    def multiply_numbers(self):
+        response = self.client.get("/multiply?value=3")  # Simulate multiplying numbers
+        if response.status_code == 200:
+            self.locust.log_success("multiply_numbers", response.elapsed.total_seconds())
+        else:
+            self.locust.log_failure("multiply_numbers", response.text)
+
+    @task
+    def divide_numbers(self):
+        response = self.client.get("/divide?value=2")  # Simulate dividing numbers
+        if response.status_code == 200:
+            self.locust.log_success("divide_numbers", response.elapsed.total_seconds())
+        else:
+            self.locust.log_failure("divide_numbers", response.text)
 
     def on_start(self):
         # Actions to perform when a user starts a test
-        pass  # Replace with your code
+        self.client.get("/")  # Visit the calculator's homepage
 
     def on_stop(self):
         # Actions to perform when a user stops a test
-        pass  # Replace with your code
+        pass  # You can add actions here if needed
