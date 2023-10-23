21032b88a8c57f0d2a3457e16ed02cf693e449d0
GaechterTimo <148241516+GaechterTimo@users.noreply.github.com>
2023-10-23 19:08:25 +0200

    Update Users.py

--- Users.py
+++ Users.py
@@ -1,65 +1,93 @@
+"""
+This is a simple calculator demo application that has external dependencies and interacts with a database.
+"""
+
+from typing import List
+
 import sqlite3
 
+
 class Calculator:
-    def __init__(self, database):
+    """A calculator that can perform basic arithmetic operations and save the results to a database."""
+
+    def __init__(self, database: Database):
         self.database = database
         self.current_value = 0
 
-    def add(self, value):
+    def add(self, value: float) -> None:
+        """Adds a value to the current value."""
         self.current_value += value
 
-    def subtract(self, value):
+    def subtract(self, value: float) -> None:
+        """Subtracts a value from the current value."""
         self.current_value -= value
 
-    def multiply(self, value):
+    def multiply(self, value: float) -> None:
+        """Multiplies the current value by a value."""
         self.current_value *= value
 
-    def divide(self, value):
+    def divide(self, value: float) -> None:
+        """Divides the current value by a value."""
+        if value == 0:
+            raise ValueError("Cannot divide by zero")
         self.current_value /= value
 
-    def get_result(self):
+    def get_result(self) -> float:
+        """Returns the current value."""
         return self.current_value
 
-    def save_result(self):
+    def save_result(self) -> None:
+        """Saves the current value to the database."""
         self.database.execute('INSERT INTO results (value) VALUES (?)', (self.current_value,))
         self.database.commit()
 
+
 class Database:
-    def __init__(self, filename):
+    """A database that stores the results of calculations."""
+
+    def __init__(self, filename: str):
         self.connection = sqlite3.connect(filename)
         self.cursor = self.connection.cursor()
 
-    def create_results_table(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS results (id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL)')
+    def create_results_table(self) -> None:
+        """Creates the results table if it doesn't exist."""
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            value REAL
+        )''')
 
-    def get_all_results(self):
+    def get_all_results(self) -> List[float]:
+        """Returns a list of all the results in the database."""
         self.cursor.execute('SELECT * FROM results')
         results = []
         for row in self.cursor.fetchall():
             results.append(row[1])
         return results
 
-database = Database('results.sqlite3')
 
-# Create the results table if it doesn't exist
-database.create_results_table()
+def main():
+    database = Database('results.sqlite3')
+    database.create_results_table()
 
-# Create a new calculator object
-calculator = Calculator(database)
+    calculator = Calculator(database)
 
-# Add two numbers
-calculator.add(2)
-calculator.add(3)
+    # Add two numbers
+    calculator.add(2)
+    calculator.add(3)
 
-# Print the result
-print(calculator.get_result())
+    # Print the result
+    print(calculator.get_result())
 
-# Save the result to the database
-calculator.save_result()
+    # Save the result to the database
+    calculator.save_result()
 
-# Get all results from the database
-results = database.get_all_results()
+    # Get all results from the database
+    results = database.get_all_results()
 
-# Print the results
-for result in results:
-    print(result)
+    # Print the results
+    for result in results:
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
