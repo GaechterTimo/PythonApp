7b397cd225961e5790e9b906aff8beaf283dbf3c
GaechterTimo <148241516+GaechterTimo@users.noreply.github.com>
2023-10-23 22:38:07 +0200

    Update locustfile.py

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,12 +1,20 @@
 from locust import HttpUser, task, between
 
-class CalculatorUser(HttpUser):
-    wait_time = between(1, 5)
+class MyUser(HttpUser):
+    wait_time = between(1, 5)  # Set wait time between tasks
 
     @task
-    def add_operation(self):
-        self.client.get("/add?value=2")
+    def visit_homepage(self):
+        self.client.get("/")
 
     @task
-    def subtract_operation(self):
-        self.client.get("/subtract?value=2")
+    def perform_search(self):
+        self.client.get("/search?query=test")
+
+    # Add more tasks as needed
+
+    def on_start(self):
+        # Actions to perform when a user starts a test
+
+    def on_stop(self):
+        # Actions to perform when a user stops a test
